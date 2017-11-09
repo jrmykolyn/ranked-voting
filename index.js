5519623cdfeeb3d94b93a515f989f1f7cf9398c9
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-11-09 09:13:41 -0500

    Fix issue where 'ballots' were not being correctly redistributed. Wrapper chunks of logic in dedicated functions. Implement 'debug' feature/option.

--- index.js
+++ index.js
@@ -2,61 +2,72 @@
 // IMPORT MODULES
 // --------------------------------------------------
 // Node
+
 // Vendor
+const meow = require( 'meow' );
+
 // Project
+
 
 // --------------------------------------------------
 // DECLARE VARS
 // --------------------------------------------------
+const cli = meow();
 
 // --------------------------------------------------
 // DECLARE FUNCTIONS
 // --------------------------------------------------
+/**
+ * Function prints arguments to stdout when process is running in `debug` mode.
+ */
+function debug() {
+    if ( cli.flags.debug ) {
+        console.log( ...( Array.prototype.slice.call( arguments, 0 ) ) );
+    }
+}
 
-// --------------------------------------------------
-// INITIALIZATION
-// --------------------------------------------------
-/// TODO:
-// - Create voters
-// - Create candidates
-// - Vote for candidates
-// - Tally votes
-// - Declare winner OR adjust ballots, redistribute, and re-tally.
+/**
+ * Wrapper around main/initialization logic.
+ */
+/// TODO: Update definition to accept `options` (eg. allow `numVoters` to be configured, etc.).
+function init() {
+    debug( 'Inside `init()`.' );
 
-// Declare vars
-let election = {};
+    let election = {};
 
-let numVoters = 100;
-let voters = [];
+    let numVoters = 100;
+    let voters = [];
 
-let numCandidates = 10;
-let candidates = {};
+    let numCandidates = 10;
+    let candidates = {};
 
-// Create 'voter' objects.
-new Array( numVoters ).fill( null ).forEach( ( el, i, arr ) => {
-    voters.push( {
-        id: i,
+    let firstNames = [ 'Sarah', 'Jen', 'Jack', 'John', 'Steve', 'Quinn', 'Bob', 'Abdi', 'Rachel' ];
+    let lastNames = [ 'Smith', 'Brown', 'Watson', 'Black', 'Johnson', 'Williams' ];
+
+    // Create 'voter' objects.
+    debug( 'Assembling voters.' );
+    new Array( numVoters ).fill( null ).forEach( ( el, i, arr ) => {
+        voters.push( {
+            id: i,
+        } );
     } );
-} );
 
-// Create 'candidate' objects.
-new Array( numCandidates ).fill( null ).forEach( ( el, i, arr ) => {
-    let candidate = {
-        id: i,
-        ballots: [],
-    };
+    // Create 'candidate' objects.
+    debug( 'Assembling candidates.' );
+    new Array( numCandidates ).fill( null ).forEach( ( el, i, arr ) => {
+        let candidate = {
+            id: i,
+            ballots: [],
+            firstName: firstNames[ Math.floor( Math.random() * firstNames.length ) ],
+            lastName: lastNames[ Math.floor( Math.random() * lastNames.length ) ],
+        };
 
-    candidates[ i ] = candidate;
-} );
+        candidates[ i ] = candidate;
+    } );
 
-// Run
-runElection( election, candidates, voters );
-
-function runElection( election, candidates, voters ) {
     // Voting
+    debug( 'Assembling ballots (eg. voting).' );
     voters.forEach( function( voter ) {
-        // console.log( 'ASSEMBLING BALLOT FOR VOTER: ', voter.id );
-
         let counter = 0;
         let candidateIds = Object.keys( candidates );
         let ballot = [];
@@ -70,27 +81,88 @@ function runElection( election, candidates, voters ) {
             }
         }
 
-        // console.log( 'BALLOT COMPLETE' );
-
-        // console.log( 'ATTACHING BALLOT TO CANDIDATE' );
-
         candidates[ ballot[ 0 ] ].ballots.push( ballot );
     } );
 
-    // Tallying
-    Object.keys( candidates ).forEach( ( k ) => {
-        // console.log( 'LOGGING OUT VOTES FOR CANDIDATE:', k );
-        // console.log( candidates[ k ].ballots.length );
+    // Run
+    debug( 'Initiating election.' );
+    let resolvedElection = runElection( election, candidates, voters );
 
-        if ( candidates[ k ].ballots.length > ( voters.length / 2 ) ) {
-            election.winner = candidates[ k ].id;
+    debug( 'Election complete, declaring winner.' );
+    console.log( `${resolvedElection.winner.firstName} ${resolvedElection.winner.lastName} won with ${resolvedElection.winner.ballots.length} votes.` );
+}
+
+/**
+ * Wrapper around election logic (tallying votes, redistributing votes, etc.).
+ *
+ * @param {Object} election
+ * @param {Object} candidates
+ * @param {Array} voters
+ * @return {Object}
+ */
+function runElection( election, candidates, voters ) {
+    debug( '\n' );
+    debug( 'Inside `runElection()`.' );
+
+    // Tallying
+    debug( 'Tallying votes.' );
+    Object.keys( candidates ).forEach( ( k ) => {
+        let candidate = candidates[ k ];
+
+        debug( `- Candidate ${candidate.id} has ${candidate.ballots.length} votes` );
+
+        if ( candidate.ballots.length > ( voters.length / 2 ) ) {
+            debug( `-- Candidate ${candidate.id} has more than 50% of the votes.` );
+            election.winner = candidate;
         }
     } );
 
     // Declaring winner
     if ( election.winner ) {
-        console.log( 'CANDIDATE WON:', election.winner );
+        debug( 'Winner determined' );
+        return election;
     } else {
-        console.log( 'NO WINNER' );
+        debug( 'Winner NOT determined' );
+        let candidateWithFewestVotes = null;
+
+        // IDENTIFY CANDIDATE WITH FEWEST VOTES
+        Object.keys( candidates ).forEach( ( k ) => {
+            let candidate = candidates[ k ];
+
+            if ( candidateWithFewestVotes === null || candidate.ballots.length < candidateWithFewestVotes.ballots.length ) {
+                candidateWithFewestVotes = candidate;
+            }
+        } );
+
+        debug( `Candidate ${candidateWithFewestVotes.id} has the fewest votes: ${candidateWithFewestVotes.ballots.length}` );
+
+        // GRAB BALLOTS FROM CANDIDATE WITH FEWEST VOTES
+        let ballotsToRedistribute = candidates[ candidateWithFewestVotes.id ].ballots;
+
+        // REMOVE CANDIDATE
+        delete candidates[ candidateWithFewestVotes.id ];
+
+        // REDISTRIBUTE BALLOTS
+        debug( 'Redistributing ballots.' );
+
+        ballotsToRedistribute.forEach( ( ballot ) => {
+            while ( !candidates[ ballot[ 0 ] ] && ballot.length ) {
+                ballot = ballot.slice( 1 );
+            }
+
+            if ( ballot.length ) {
+                debug( `- Redistributing ballot to candidate ${ballot[ 0 ]}` );
+
+                candidates[ ballot[ 0 ] ].ballots.push( ballot );
+            }
+        } );
+
+        // DO IT AGAIN
+        return runElection( election, candidates, voters );
     }
 }
+
+// --------------------------------------------------
+// INITIALIZATION
+// --------------------------------------------------
+init();
